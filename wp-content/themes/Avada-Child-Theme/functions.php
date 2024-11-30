e6678db52ac46d8e32953583f68e606d8a64ecf4
Sylwia <chedkowska0@gmail.com>
2024-11-30 21:38:24 +0100

    fix css issues

---
+++ wp-content/themes/Avada-Child-Theme/functions.php
@@ -0,0 +1,16 @@
+<?php
+
+function theme_enqueue_styles()
+{
+    wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/style.css', []);
+}
+add_action('wp_enqueue_scripts', 'theme_enqueue_styles', 20);
+
+function avada_lang_setup()
+{
+    $lang = get_stylesheet_directory() . '/languages';
+    load_child_theme_textdomain('Avada', $lang);
+}
+
+
+
